2a233a3e2cb1df162e97aeda48cb969e124cecc0
Renan Cunha <renancdev@gmail.com>
2022-05-04 11:58:58 -0300

    Mudando de JS para TS

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButton = document.querySelector('[data-equal]');
-const deleteButton = document.querySelector('[data-clear]');
-const allClearButton = document.querySelector('[data-all-clear]');
-const previousNumber = document.querySelector('[data-previous-number]');
-const currentNumber = document.querySelector('[data-current-number]');
