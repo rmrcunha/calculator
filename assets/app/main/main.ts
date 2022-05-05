573e9710f983d5e57f39e50a7df8045ace21966e
Renan Cunha <renancdev@gmail.com>
2022-05-05 10:13:41 -0300

    Calculadora pronta para uso

--- assets/app/main/main.ts
+++ assets/app/main/main.ts
@@ -1,7 +1,40 @@
+import { Calculator } from "./result.js";
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equal]');
 const deleteButton = document.querySelector('[data-clear]');
 const allClearButton = document.querySelector('[data-all-clear]');
-const previousNumber = document.querySelector('[data-previous-number]');
-const currentNumber = document.querySelector('[data-current-number]');
+const previousOperandTextElement = document.querySelector('[data-previous-number]');
+const currentOperandTextElement = document.querySelector('[data-current-number]');
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText)
+      calculator.updateDisplay();
+    })
+  })
+
+equalsButton.addEventListener('click', button =>{
+    calculator.compute();
+    calculator.updateDisplay();
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener('click',button =>{
+    calculator.delete();
+    calculator.updateDisplay();
+})
+
+console.log(operationButtons)
---
+++ assets/app/main/result.ts
@@ -0,0 +1,75 @@
+export class Calculator{
+    previousOperandTextElement: any;
+    currentOperandTextElement: any;
+    currentOperand: string;
+    previousOperand: string;
+    operation: any;
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+    clear(){
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+    delete(){
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
+    appendNumber(number) {
+        if(number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+    chooseOperation(operation){
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+
+    }
+    compute(){
+        let computation;
+        const previous = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(previous) || isNaN(current)) return
+        switch (this.operation){
+            case '+' :
+                computation = previous + current;
+                break
+            case '-' :
+                computation = previous - current;
+                break
+            case '*':
+                computation = previous * current;
+                break
+            case '÷':
+                computation = previous / current;
+                break
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+    getDisplayNumber(number){
+        const floatNumber = parseFloat(number);
+        if (isNaN(floatNumber)) return ''
+        return floatNumber.toLocaleString('en')
+    }
+
+    updateDisplay(){
+        this.currentOperandTextElement.innerText =
+        this.getDisplayNumber(this.currentOperand)
+        if (this.operation !== null) {
+            this.previousOperandTextElement.innerText = 
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = '';
+        }
+    }
+} 
